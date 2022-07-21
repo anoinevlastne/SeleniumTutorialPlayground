08d1db24c0be85ea545b344eab734574f08d1ad9
anoinevlastne <urych.s@gmail.com>
2022-07-21 18:04:29 +0200

    100%, pridal jsem class Maintest a nejaky classy z toho dedi

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTest {
+
+    public WebDriver driver;
+
+    @Before
+    public void setUpDriver(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDownDriver(){
+        driver.close();
+        driver.quit();
+    }
+}
---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        CalcComplete.class,
+        CalcSumTest.class,
+        CalcMinusTest.class,
+        ManageWindow.class,
+        RegistrationComplete.class,
+}
+)
+
+
+public class TestSuite {
+}
